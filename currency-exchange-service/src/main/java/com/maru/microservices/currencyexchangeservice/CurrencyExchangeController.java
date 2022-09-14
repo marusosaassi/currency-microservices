0fe3bad5b7f06b7e2e2d1cb191f112be0122d087
Maru Sosa <soassi@outlook.com>
2022-09-14 07:59:49 -0500

    setting up dynamic port in the response

--- currency-exchange-service/src/main/java/com/maru/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/maru/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -2,16 +2,24 @@ package com.maru.microservices.currencyexchangeservice;
 
 import java.math.BigDecimal;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CurrencyExchangeController {
+	@Autowired
+	private Environment environment;
+	
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	public CurrencyExchange retrieveExchangeValue(
 			@PathVariable String from, @PathVariable String to
 			) {
-		return new CurrencyExchange(100L, from, to, BigDecimal.valueOf(50));
+		CurrencyExchange currencyExchange = new CurrencyExchange(100L, from, to, BigDecimal.valueOf(50));
+		String port = environment.getProperty("local.server.port");
+		currencyExchange.setEnvironment(port);
+		return currencyExchange;
 	}
 }
