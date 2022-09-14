169f98732cf2def9f6858bfb9d3365ef899f264b
Maru Sosa <soassi@outlook.com>
2022-09-14 08:54:13 -0500

    jpa repository for the currency exchange service

---
+++ currency-exchange-service/src/main/java/com/maru/microservices/currencyexchangeservice/CurrencyExchangeRepository.java
@@ -0,0 +1,9 @@
+package com.maru.microservices.currencyexchangeservice;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CurrencyExchangeRepository 
+	extends JpaRepository<CurrencyExchange, Long>{
+	
+	CurrencyExchange findByFromAndTo (String from, String to);
+}
