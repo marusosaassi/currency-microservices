690bcfb148d464f83827f8873666474fd4b0c8ff
Maru Sosa <soassi@outlook.com>
2022-09-14 08:38:46 -0500

    jpa configuration and initialized data

--- currency-exchange-service/src/main/java/com/maru/microservices/currencyexchangeservice/CurrencyExchange.java
+++ currency-exchange-service/src/main/java/com/maru/microservices/currencyexchangeservice/CurrencyExchange.java
@@ -2,9 +2,18 @@ package com.maru.microservices.currencyexchangeservice;
 
 import java.math.BigDecimal;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class CurrencyExchange {
+	
+	@Id
 	private long id;
+	@Column(name = "currency_from")
 	private String from;
+	@Column(name = "currency_to")
 	private String to;
 	private BigDecimal conversionMultiple;
 	private String environment;
---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,39 @@
+insert into currency_exchange 
+(id, currency_from, currency_to, conversion_multiple, environment)
+values (10001, 'USD', 'INR', 65, '');
+
+insert into currency_exchange 
+(id, currency_from, currency_to, conversion_multiple, environment)
+values (10002, 'USD', 'EUR', 1, '');
+
+insert into currency_exchange 
+(id, currency_from, currency_to, conversion_multiple, environment)
+values (10003, 'USD', 'MXN', 20.03, '');
+
+insert into currency_exchange 
+(id, currency_from, currency_to, conversion_multiple, environment)
+values (10004, 'EUR', 'MXN', 20.02, '');
+
+insert into currency_exchange 
+(id, currency_from, currency_to, conversion_multiple, environment)
+values (10005, 'RUB', 'MXN', 0.34, '');
+
+insert into currency_exchange 
+(id, currency_from, currency_to, conversion_multiple, environment)
+values (10006, 'PEN', 'MXN', 5.19, '');
+
+insert into currency_exchange 
+(id, currency_from, currency_to, conversion_multiple, environment)
+values (10007, 'GBP', 'MXN', 23.17, '');
+
+insert into currency_exchange 
+(id, currency_from, currency_to, conversion_multiple, environment)
+values (10008, 'MXN', 'VEF', 0.40, '');
+
+insert into currency_exchange 
+(id, currency_from, currency_to, conversion_multiple, environment)
+values (10009, 'MXN', 'JPY', 7.13, '');
+
+insert into currency_exchange 
+(id, currency_from, currency_to, conversion_multiple, environment)
+values (10010, 'MXN', 'KRW', 69.53, '');
